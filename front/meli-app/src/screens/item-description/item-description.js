ec218d40bb7cdecf7355a4b511c28d6486b164dd
JuliannyRpoL <45438216+JuliannyRpoL@users.noreply.github.com>
2022-05-18 00:19:16 -0500

    added lazy loading

--- front/meli-app/src/screens/item-description/item-description.js
+++ front/meli-app/src/screens/item-description/item-description.js
@@ -2,18 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 
 import Search from '../../components/search/search'
-import ItemDetails from './components/item-details/item-details';
 import Breadcrumb from '../../components/breadcrumb/breadcrumb';
 
 import { getItemDetailsApi } from "../../services/items.service";
 
 import "./item-description.scss"
 
+const ItemDetailsComponent = React.lazy(() => import('./components/item-details/item-details'))
+
 export default function ItemDescription() {
     const [item, setItem] = useState(null);
     const { id } = useParams();
 
-    useEffect( () => {
+    useEffect(() => {
         getItemDetailsApi(id).then((res) => {
             setItem(res['item'])
         });
@@ -29,7 +30,7 @@ export default function ItemDescription() {
                     {item ? <Breadcrumb categories={item.categories}></Breadcrumb> : null }
                 </div>
                 <div className='item-description__body__item'>
-                    {item ? <ItemDetails details={item}></ItemDetails> : null}
+                    {item ? <ItemDetailsComponent details={item}></ItemDetailsComponent> : null}
                 </div>
             </main>
         </section>
--- front/meli-app/src/screens/results/results.js
+++ front/meli-app/src/screens/results/results.js
@@ -4,18 +4,19 @@ import React, { useState, useEffect } from 'react'
 import { useLocation } from "react-router-dom";
 
 import Search from '../../components/search/search'
-import Item from './components/item/item'
 import Breadcrumb from '../../components/breadcrumb/breadcrumb';
 
 import { getItemsApi } from "../../services/items.service";
 
 import "./results.scss"
 
+const ItemComponent = React.lazy(() => import('./components/item/item'))
+
 export default function Results() {
     const [items, setItems] = useState(null);
     const search = new URLSearchParams(useLocation().search).get("search");
 
-    useEffect( () => {
+    useEffect(() => {
         getItemsApi(search).then((res) => {
             setItems(res)
         });
@@ -32,7 +33,7 @@ export default function Results() {
                 </div>
                 <div className='results__body__items'>
                     {items && items.items.map((item) => {
-                        return <Item data={item} key={item['id']}></Item>
+                        return <ItemComponent data={item} key={item['id']}></ItemComponent>
                     })}
                 </div>
             </main>
